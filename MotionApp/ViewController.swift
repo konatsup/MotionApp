4ec2478ed8319be577e8e12ae35cb29516b6f366
konatsup <konatsukomatsu@gmail.com>
2020-03-02 13:50:03 +0900

    SpreadsheetViewの導入

--- MotionApp/ViewController.swift
+++ MotionApp/ViewController.swift
@@ -12,8 +12,21 @@ import RxCocoa
 import SnapKit
 import SpreadsheetView
 
-class ViewController: UIViewController, SpreadsheetViewDataSource {
-    var spreadsheetView: SpreadsheetView!
+class ViewController: UIViewController, SpreadsheetViewDelegate,SpreadsheetViewDataSource {
+//    var spreadsheetView: SpreadsheetView!
+    private lazy var spreadSheetView: SpreadsheetView = {
+        let spreadSheetView = SpreadsheetView()
+        spreadSheetView.translatesAutoresizingMaskIntoConstraints = false
+        spreadSheetView.backgroundColor = .blue
+        spreadSheetView.dataSource = self
+        spreadSheetView.delegate = self
+        spreadSheetView.stickyRowHeader = true
+        spreadSheetView.stickyColumnHeader = true
+//        spreadSheetView.circularScrolling = CircularScrolling.Configuration.both
+        spreadSheetView.register(HeaderCell.self, forCellWithReuseIdentifier: String(describing: HeaderCell.self))
+        spreadSheetView.register(TextCell.self, forCellWithReuseIdentifier: String(describing: TextCell.self))
+        return spreadSheetView
+    }()
     var drawView: DrawView!
     
     private let disposeBag = DisposeBag()
@@ -37,13 +50,13 @@ class ViewController: UIViewController, SpreadsheetViewDataSource {
         button.setTitle("+", for: .normal)
         view.addSubview(button)
         
-        button.snp.makeConstraints { (make) in
-            make.top.equalTo(600)
-            make.center.equalTo(self.view.center)
-            make.width.equalTo(200)
-            make.height.equalTo(50)
-        }
-        
+//        button.snp.makeConstraints { (make) in
+//            make.top.equalTo(600)
+//            make.center.equalTo(self.view.center)
+//            make.width.equalTo(200)
+//            make.height.equalTo(50)
+//        }
+//
         button.rx.tap
             .bind(to: viewModel.btnTapped)
             .disposed(by: disposeBag)
@@ -52,12 +65,12 @@ class ViewController: UIViewController, SpreadsheetViewDataSource {
         label.text = "aaaa"
         self.view.addSubview(label)
         
-        label.snp.makeConstraints { (make) in
-            make.top.equalTo(button.snp.top).offset(-100)
-            make.center.equalTo(self.view.center)
-            make.width.equalTo(100)
-            make.height.equalTo(20)
-        }
+//        label.snp.makeConstraints { (make) in
+//            make.top.equalTo(button.snp.top).offset(-100)
+//            make.center.equalTo(self.view.center)
+//            make.width.equalTo(100)
+//            make.height.equalTo(20)
+//        }
         
         
 //        spreadsheetView = SpreadsheetView()
@@ -65,17 +78,18 @@ class ViewController: UIViewController, SpreadsheetViewDataSource {
 //        spreadsheetView.backgroundColor = UIColor.red
 //        spreadsheetView.register(HeaderCell.self, forCellWithReuseIdentifier: HeaderCell.reuseIdentifier)
 //        spreadSheetView.register(TextCell.self, forCellWithReuseIdentifier: TextCell.reuseIdentifier)
-//        self.view.addSubview(spreadsheetView
-//        spreadsheetView.snp.makeConstraints { (make) -> Void in
-//            make.width.height.equalTo(100)
-//            make.center.equalTo(self.view)
-//        }
+        self.view.addSubview(spreadSheetView)
+        spreadSheetView.snp.makeConstraints { (make) -> Void in
+            make.bottom.equalTo(self.view.snp.bottom)
+            make.height.equalTo(self.view.snp.height).dividedBy(2)
+            make.width.equalTo(self.view.snp.width)
+            make.center.equalTo(self.view.snp.center)
+        }
         
         drawView = DrawView()
         self.view.addSubview(drawView)
         drawView.snp.makeConstraints { (make) -> Void in
             make.top.equalTo(self.view.snp.top)
-            make.left.equalTo(self.view.snp.left)
             make.width.equalTo(self.view.snp.width)
             make.height.equalTo(self.view.snp.height).dividedBy(2)
             make.center.equalTo(self.view.snp.center)
@@ -85,11 +99,11 @@ class ViewController: UIViewController, SpreadsheetViewDataSource {
     }
     
     func numberOfColumns(in spreadsheetView: SpreadsheetView) -> Int {
-        return 200
+        return 20
     }
     
     func numberOfRows(in spreadsheetView: SpreadsheetView) -> Int {
-        return 400
+        return 40
     }
     
     func spreadsheetView(_ spreadsheetView: SpreadsheetView, widthForColumn column: Int) -> CGFloat {
@@ -103,8 +117,6 @@ class ViewController: UIViewController, SpreadsheetViewDataSource {
         if case 0 = indexPath.row {
             let cell = spreadsheetView.dequeueReusableCell(withReuseIdentifier: String(describing: HeaderCell.self), for: indexPath) as! HeaderCell
             cell.label.text = "aaaa"
-            
-            
             cell.sortArrow.text = "uuuuu"
             
             cell.setNeedsLayout()
@@ -112,7 +124,7 @@ class ViewController: UIViewController, SpreadsheetViewDataSource {
             return cell
         } else {
             let cell = spreadsheetView.dequeueReusableCell(withReuseIdentifier: String(describing: TextCell.self), for: indexPath) as! TextCell
-            cell.label.text = "iiii"
+            cell.label.text = "iiiiadsfafsfsdfas"
             return cell
         }
     }
