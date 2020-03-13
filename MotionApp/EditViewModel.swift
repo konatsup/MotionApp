b783cb01cc80bb8f0a43ae3116e4c6fc24787cc7
konatsup <konatsukomatsu@gmail.com>
2020-03-13 14:34:00 +0900

    firebaseにアップロードできるようになった

--- MotionApp/EditViewModel.swift
+++ MotionApp/EditViewModel.swift
@@ -9,6 +9,7 @@
 import Foundation
 import RxSwift
 import RxCocoa
+import Firebase
 
 protocol ViewModelInput {
     var btnTapped: PublishRelay<Void> { get }
@@ -26,6 +27,8 @@ struct Entity {
 final class EditViewModel: ViewModelInput, ViewModelOutput {
     private let disposeBag = DisposeBag()
     
+    var databaseRef: DatabaseReference!
+    
     //input
     var btnTapped = PublishRelay<Void>()
     var uploadBtnTapped = PublishRelay<Void>()
@@ -39,6 +42,13 @@ final class EditViewModel: ViewModelInput, ViewModelOutput {
     var timerCount: Double = 0.0
     
     init() {
+        databaseRef = Database.database().reference()
+        databaseRef.observe(.childAdded, with: { snapshot in
+            if let obj = snapshot.value as? [String : AnyObject], let name = obj["name"] as? String, let message = obj["message"] {
+                //                let currentText = self.textView.text
+                //                self.textView.text = (currentText ?? "") + "\n\(name) : \(message)"
+            }
+        })
         btnTapped.bind(to: Binder(self) {me, _ in
             
             if !self.timer.isValid {
@@ -55,6 +65,18 @@ final class EditViewModel: ViewModelInput, ViewModelOutput {
         
         uploadBtnTapped.bind(to: Binder(self) {me, _ in
             print("upload")
+            
+            var animations: [AnimationLayer] = []
+            let animation1 = AnimationLayer(startTime: 1.0, endTime: 2.0, fromX: 100, fromY: 100, toX: 300, toY: 600)
+            let animation2 = AnimationLayer(startTime: 3.0, endTime: 5.0, fromX: 0, fromY: 800, toX: 400, toY: 200)
+            animations.append(animation1)
+            animations.append(animation2)
+            
+            for animation in animations {
+                let animationData = ["startTime": animation.startTime, "endTime": animation.endTime, "fromX": animation.fromX, "fromY": animation.fromY, "toX": animation.toX, "toY": animation.toY]
+                self.databaseRef.child("animations") .childByAutoId().setValue(animationData)
+            }
+            
         }).disposed(by: disposeBag)
         
         
