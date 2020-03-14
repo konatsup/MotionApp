9cf1bf0e56a5412db669be6b2a7a7a49acf76bb9
konatsup <konatsukomatsu@gmail.com>
2020-03-14 19:15:16 +0900

    タイムラインのレイアウト修正

--- MotionApp/EditViewController.swift
+++ MotionApp/EditViewController.swift
@@ -10,29 +10,9 @@ import UIKit
 import RxSwift
 import RxCocoa
 import SnapKit
-import SpreadsheetView
-//import Firebase
 
 class EditViewController: UIViewController{
-    //    var spreadsheetView: SpreadsheetView!
-    
     private lazy var dataSource = EditViewDataSource()
-    //        private lazy var spreadSheetView: SpreadsheetView = {
-    //            let spreadSheetView = SpreadsheetView()
-    //            spreadSheetView.translatesAutoresizingMaskIntoConstraints = false
-    //            spreadSheetView.backgroundColor = .white
-    //            spreadSheetView.dataSource = dataSource
-    //            spreadSheetView.delegate = self
-    //            spreadSheetView.stickyRowHeader = true
-    //            spreadSheetView.stickyColumnHeader = true
-    //            spreadSheetView.bounces = false
-    //            spreadSheetView.isDirectionalLockEnabled = true
-    //            //        spreadSheetView.gridStyle = .default
-    //            spreadSheetView.register(HeaderCell.self, forCellWithReuseIdentifier: String(describing: HeaderCell.self))
-    //            spreadSheetView.register(TimeMeterCell.self, forCellWithReuseIdentifier: String(describing: TimeMeterCell.self))
-    //            spreadSheetView.register(TextCell.self, forCellWithReuseIdentifier: String(describing: TextCell.self))
-    //            return spreadSheetView
-    //        }()
     
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: self.view.bounds, style: .plain)
@@ -40,15 +20,28 @@ class EditViewController: UIViewController{
         tableView.dataSource = self
         tableView.backgroundColor = .darkGray
         tableView.allowsSelection = true
-//        tableView.isEditing = true
-//        tableView.allowsSelectionDuringEditing = false
+        //        tableView.isEditing = true
+        //        tableView.allowsSelectionDuringEditing = false
         tableView.register(UINib(nibName: "TrackViewCell", bundle: nil), forCellReuseIdentifier: "TrackViewCell")
         return tableView
     }()
     
     var drawView: DrawView!
     
-    private lazy var scrollView: UIScrollView = {
+    private lazy var timeMeterView: UIScrollView = {
+        let timeMeterView = UIScrollView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.timeMeterHeight))
+        timeMeterView.delegate = self
+        timeMeterView.backgroundColor = .lightGray
+        timeMeterView.showsHorizontalScrollIndicator = false
+        timeMeterView.showsVerticalScrollIndicator = false
+        timeMeterView.isDirectionalLockEnabled = true
+        let width = self.view.frame.maxX
+        timeMeterView.contentSize = CGSize(width:CGFloat(self.pageSize) * width, height: self.timeMeterHeight)
+        
+        return timeMeterView
+    }()
+    
+    private lazy var scrollViewMain: UIScrollView = {
         let scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height/2))
         scrollView.delegate = self
         scrollView.backgroundColor = .blue
@@ -56,9 +49,7 @@ class EditViewController: UIViewController{
         scrollView.showsVerticalScrollIndicator = false
         scrollView.isDirectionalLockEnabled = true
         let width = self.view.frame.maxX
-        let pageSize = 10
-        scrollView.contentSize = CGSize(width:CGFloat(pageSize) * width, height:4000)
-        
+        scrollView.contentSize = CGSize(width:CGFloat(self.pageSize) * width, height:4000)
         
         return scrollView
     }()
@@ -74,7 +65,12 @@ class EditViewController: UIViewController{
     var scrollViews: [UIScrollView] = []
     var trackViewCells: [TrackViewCell] = []
     
-    let timeMeterHeight = 50
+    let timeMeterHeight: CGFloat = 50
+    let buttonHeight = 50
+    let buttonWidth = 80
+    let pageSize = 10
+    let sideCellWidth: CGFloat = 50.0
+    
     
     init(project: Project) {
         self.project = project
@@ -90,7 +86,6 @@ class EditViewController: UIViewController{
         let p = Project(animations: animations)
         self.project = p
         super.init(coder: aDecoder)
-        //        fatalError("init(coder:) has not been implemented")
     }
     
     override func viewDidLoad() {
@@ -125,7 +120,6 @@ class EditViewController: UIViewController{
             .asDriver(onErrorJustReturn: "")
             .drive(Binder(self) {me, state in
                 print(state)
-                //                me.navigationController?.popViewController(animated: true)
                 self.dismiss(animated: true, completion: nil)
             })
             .disposed(by: disposeBag)
@@ -140,17 +134,6 @@ class EditViewController: UIViewController{
             })
             .disposed(by: disposeBag)
         
-        //        self.view.addSubview(spreadSheetView)
-        //        spreadSheetView.snp.makeConstraints { (make) -> Void in
-        //            make.bottom.equalTo(self.view.snp.bottom)
-        //            make.height.equalTo(self.view.snp.height).dividedBy(2)
-        //            make.width.equalTo(self.view.snp.width)
-        //            make.center.equalTo(self.view.snp.center)
-        //        }
-        //        spreadSheetView.reloadData()
-        
-        
-        
         drawView = DrawView()
         self.view.addSubview(drawView)
         
@@ -158,7 +141,6 @@ class EditViewController: UIViewController{
             make.top.equalTo(self.view.snp.top)
             make.width.equalTo(self.view.snp.width)
             make.height.equalTo(self.view.snp.height).dividedBy(2)
-            make.center.equalTo(self.view.snp.center)
         }
         
         self.view.addSubview(tableView)
@@ -166,35 +148,54 @@ class EditViewController: UIViewController{
             make.bottom.equalTo(self.view.snp.bottom)
             make.height.equalTo(self.view.snp.height).dividedBy(2).offset(-timeMeterHeight)
             make.width.equalTo(self.view.snp.width)
-            make.center.equalTo(self.view.snp.center)
         }
         
-//        self.view.addSubview(scrollView)
-//        scrollView.snp.makeConstraints { (make) -> Void in
-//            make.bottom.equalTo(self.view.snp.bottom)
-//            make.height.equalTo(self.view.snp.height).dividedBy(2)
-//            make.width.equalTo(self.view.snp.width)
-//
-//            make.left.equalTo(self.view.snp.left).offset(50.0)
-//            make.center.equalTo(self.view.snp.center)
-//        }
+        self.view.addSubview(scrollViewMain)
+        scrollViewMain.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(drawView.snp.bottom)
+            make.bottom.equalTo(self.view.snp.bottom)
+            make.height.equalTo(self.view.snp.height).dividedBy(2).offset(-timeMeterHeight)
+            make.width.equalTo(self.view.snp.width)
+            make.left.equalTo(self.view.snp.left).offset(sideCellWidth)
+        }
         
-        let pageSize = 10
+        self.view.addSubview(timeMeterView)
+        timeMeterView.snp.makeConstraints { (make) -> Void in
+            make.bottom.equalTo(scrollViewMain.snp.top)
+            make.height.equalTo(self.timeMeterHeight)
+            make.width.equalTo(self.view.snp.width)
+            make.left.equalTo(self.view.snp.left).offset(sideCellWidth)
+        }
+        
         let width = self.view.frame.width
-        for j in 0 ..< 100 {
-            for i in 0 ..< pageSize {
+        for j in 0 ..< 20 {
+            for i in 0 ..< self.pageSize {
                 //ページごとに異なるラベルを表示.
                 let myLabel:UILabel = UILabel(frame: CGRect(x:CGFloat(i)*width/4, y:CGFloat(j)*50, width:80, height:50))
                 myLabel.backgroundColor = UIColor.red
                 myLabel.textColor = UIColor.white
                 myLabel.textAlignment = NSTextAlignment.center
                 myLabel.layer.masksToBounds = true
-                myLabel.text = "Page\(i)"
+                myLabel.text = "Page\(j) : \(i)"
                 myLabel.font = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
                 myLabel.layer.cornerRadius = 30.0
                 
-                scrollView.addSubview(myLabel)
+                scrollViewMain.addSubview(myLabel)
             }
+        }
+        
+        for i in 0 ..< self.pageSize {
+            //ページごとに異なるラベルを表示.
+            let myLabel:UILabel = UILabel(frame: CGRect(x:CGFloat(i)*width/4, y: 0, width:80, height: timeMeterHeight))
+            myLabel.backgroundColor = UIColor.yellow
+            myLabel.textColor = UIColor.white
+            myLabel.textAlignment = NSTextAlignment.center
+            myLabel.layer.masksToBounds = true
+            myLabel.text = "Page\(i)"
+            myLabel.font = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
+            myLabel.layer.cornerRadius = 30.0
+            
+            timeMeterView.addSubview(myLabel)
         }
         
         let button = UIButton()
@@ -203,25 +204,25 @@ class EditViewController: UIViewController{
         view.addSubview(button)
         
         button.snp.makeConstraints { (make) in
-            make.top.equalTo(400)
-            make.left.equalTo(100)
-            make.width.equalTo(50)
-            make.height.equalTo(20)
+            make.top.equalTo(self.drawView.snp.bottom).offset(-buttonHeight)
+            make.left.equalTo(buttonWidth * 2)
+            make.width.equalTo(buttonWidth)
+            make.height.equalTo(buttonHeight)
         }
         button.rx.tap
             .bind(to: viewModel.btnTapped)
             .disposed(by: disposeBag)
-        
+        //
         let stopButton = UIButton()
         stopButton.backgroundColor = .orange
         stopButton.setTitle("stop", for: .normal)
         view.addSubview(stopButton)
         
         stopButton.snp.makeConstraints { (make) in
-            make.top.equalTo(400)
-            make.left.equalTo(150)
-            make.width.equalTo(50)
-            make.height.equalTo(20)
+            make.bottom.equalTo(self.drawView.snp.bottom)
+            make.left.equalTo(buttonWidth * 3)
+            make.width.equalTo(buttonWidth)
+            make.height.equalTo(buttonHeight)
         }
         stopButton.rx.tap
             .bind(to: viewModel.stopBtnTapped)
@@ -234,10 +235,10 @@ class EditViewController: UIViewController{
         view.addSubview(uploadButton)
         
         uploadButton.snp.makeConstraints { (make) in
-            make.top.equalTo(400)
-            make.left.equalTo(0)
-            make.width.equalTo(50)
-            make.height.equalTo(20)
+            make.bottom.equalTo(self.drawView.snp.bottom)
+            make.left.equalTo(buttonWidth * 0)
+            make.width.equalTo(buttonWidth)
+            make.height.equalTo(buttonHeight)
         }
         
         uploadButton.rx.tap
@@ -250,10 +251,10 @@ class EditViewController: UIViewController{
         view.addSubview(testButton)
         
         testButton.snp.makeConstraints { (make) in
-            make.top.equalTo(400)
-            make.left.equalTo(50)
-            make.width.equalTo(50)
-            make.height.equalTo(20)
+            make.bottom.equalTo(self.drawView.snp.bottom)
+            make.left.equalTo(buttonWidth * 1)
+            make.width.equalTo(buttonWidth)
+            make.height.equalTo(buttonHeight)
         }
         
         testButton.rx.tap
@@ -265,7 +266,7 @@ class EditViewController: UIViewController{
         self.view.addSubview(label)
         
         label.snp.makeConstraints { (make) in
-            make.top.equalTo(button.snp.top).offset(-100)
+            make.top.equalTo(200)
             make.center.equalTo(self.view.center)
             make.width.equalTo(100)
             make.height.equalTo(20)
@@ -305,7 +306,7 @@ extension EditViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return 20
     }
     
     func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
@@ -324,17 +325,17 @@ extension EditViewController: UITableViewDataSource, UITableViewDelegate {
         return false
     }
     
-//    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-//        scrollBeginingPoint = scrollView.contentOffset;
-//    }
-//
-//    func scrollViewDidScroll(scrollView: UIScrollView) {
-//        let currentPoint = scrollView.contentOffset
-//        print(currentPoint)
-//        if scrollBeginingPoint.y != currentPoint.y {
-//            self.scrollView.contentOffset.y = currentPoint.y
-//        }
-//    }
+    //    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+    //        scrollBeginingPoint = scrollView.contentOffset;
+    //    }
+    //
+    //    func scrollViewDidScroll(scrollView: UIScrollView) {
+    //        let currentPoint = scrollView.contentOffset
+    //        print(currentPoint)
+    //        if scrollBeginingPoint.y != currentPoint.y {
+    //            self.scrollView.contentOffset.y = currentPoint.y
+    //        }
+    //    }
     
     
     //    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
@@ -351,23 +352,32 @@ extension EditViewController: UIScrollViewDelegate {
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
         print("endDragging")
     }
-
+    
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        guard scrollView == self.scrollView else { return }
+        //        guard scrollView == self.scrollView else { return }
         print("didEndDecelerating")
     }
     //
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
         scrollBeginingPoint = scrollView.contentOffset
     }
-
+    
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         print("scrollViewDidScroll")
         //        print(scrollView.contentOffset)
         let currentPoint = scrollView.contentOffset
-        if scrollBeginingPoint.y != currentPoint.y {
-            self.tableView.contentOffset.y = currentPoint.y
+        
+        if scrollView == timeMeterView {
+            if scrollBeginingPoint.x != currentPoint.x {
+                self.scrollViewMain.contentOffset.x = currentPoint.x
+            }
+        } else if scrollView == scrollViewMain {
+            if scrollBeginingPoint.y != currentPoint.y {
+                self.tableView.contentOffset.y = currentPoint.y
+            } else if scrollBeginingPoint.x != currentPoint.x {
+                self.timeMeterView.contentOffset.x = currentPoint.x
+            }
         }
-
+        
     }
 }
--- MotionApp/TrackViewCell.swift
+++ MotionApp/TrackViewCell.swift
@@ -11,8 +11,6 @@ import SnapKit
 
 class TrackViewCell: UITableViewCell, UIScrollViewDelegate {
     var label: UILabel = UILabel()
-//    var scrollView: UIScrollView = UIScrollView()
-//    var viewModel: EditViewModel = EditViewModel()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -29,43 +27,9 @@ class TrackViewCell: UITableViewCell, UIScrollViewDelegate {
     }
     
     func setCell() {
-//        scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: self.contentView.frame.width, height: 100))
-//        scrollView.delegate = self
-//        scrollView.backgroundColor = .blue
-//        scrollView.showsHorizontalScrollIndicator = false
-//        scrollView.showsVerticalScrollIndicator = false
-//
-//        let width = self.contentView.frame.maxX
-//        let pageSize = 10
-//        scrollView.contentSize = CGSize(width:CGFloat(pageSize) * width, height:0)
-//
-//        self.contentView.addSubview(scrollView)
-//
-//        scrollView.snp.makeConstraints { (make) -> Void in
-//            make.top.equalTo(self.contentView.snp.top)
-//            make.width.equalTo(self.contentView.snp.width)
-//            make.height.equalTo(self.contentView.snp.height)
-//            make.center.equalTo(self.contentView.snp.center)
-//        }
-        
-//        print(width)
-//        for i in 0 ..< pageSize {
-//
-//            //ページごとに異なるラベルを表示.
-//            let myLabel:UILabel = UILabel(frame: CGRect(x:CGFloat(i)*width/4, y:0, width:80, height:20))
-//            myLabel.backgroundColor = UIColor.red
-//            myLabel.textColor = UIColor.white
-//            myLabel.textAlignment = NSTextAlignment.center
-//            myLabel.layer.masksToBounds = true
-//            myLabel.text = "Page\(i)"
-//            myLabel.font = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
-//            myLabel.layer.cornerRadius = 30.0
-//
-//            scrollView.addSubview(myLabel)
-//        }
         
         label = UILabel()
-        //        label.text = "aaaaaa"
+        label.text = "aaaaaa"
         label.font = label.font.withSize(50)
         self.contentView.addSubview(label)
         label.snp.makeConstraints { (make) -> Void in
@@ -74,9 +38,7 @@ class TrackViewCell: UITableViewCell, UIScrollViewDelegate {
             make.height.equalTo(self.contentView.snp.height)
             //            make.center.equalTo(self.contentView.snp.center)
         }
-        
-        //        scrollViewList.append(scrollView)
-        
+                
     }
     
 }
